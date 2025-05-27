cf09794b535d47decd3eacb1a9b7715195947b7b
Artyom <amoiseev_00@mail.ru>
2025-05-28 01:19:09 +0300

    add tests config

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 10000,
   },
 });
